c13791149bd5020ef6b0b93817fcd79c6f47e5b8
Logan Kuzyk <logan@kuzyk.net>
2020-07-14 19:08:01 -0700

    Basic website, nothing to make requests to backend yet though

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("site/index", {
+    title: "shareddit: share reddit posts with images",
+    info: fs.readFileSync(__dirname + "/../views/site/info.hbs"),
+  });
 });
 
 module.exports = router;
