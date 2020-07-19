5855e70ac7bd87f91f50fc1de240401e8f7980cb
Logan Kuzyk <logan@kuzyk.net>
2020-07-18 18:12:56 -0700

    Add socket.io to load image after it's done generating

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,29 @@ const dotenv = require("dotenv").config();
 const rp = require("request-promise");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res, next) => {
   res.render("site/index", {
     title: "shareddit: share reddit posts with images",
     info: fs.readFileSync(__dirname + "/../views/site/info.hbs"),
   });
 });
 
-router.get("/image/:ID", async (req, res, next) => {
-  let image = null;
-  let body = await handlebars.compile(
-    fs.readFileSync(__dirname + "/../views/site/generated.hbs", "utf8")
-  );
-  body = await body({ link: data.url });
-  await rp(process.env.BACKEND_URL + req.originalUrl).then((response) => {
-    console.log(response);
-    image = response.image;
-    console.log("received " + image);
-  });
-  res.render("site/index", async () => {
-    title: "shareddit: generated image";
-    link: image;
-  });
-});
+// router.get("/image/:ID", async (req, res, next) => {
+//   let image = null;
+//   let body = await handlebars.compile(
+//     fs.readFileSync(__dirname + "/../views/site/generated.hbs", "utf8")
+//   );
+//   body = await body({ link: data.url });
+//   await rp(process.env.BACKEND_URL + req.originalUrl).then((response) => {
+//     console.log(response);
+//     image = response.image;
+//     console.log("received " + image);
+//   });
+//   res.render("site/index", async () => {
+//     title: "shareddit: generated image";
+//     link: image;
+//   });
+// });
 
 router.get(
   "/r/:sub/comments/:postID/:title?/:commentID?",
@@ -44,11 +44,13 @@ router.get(
       id = req.params.postID;
     }
     console.log(req.originalUrl);
+    const io = req.app.get("socketio");
     await rp(process.env.BACKEND_URL + req.originalUrl).then((response) => {
-      console.log(response);
+      //send image url to client
       image = response.image;
+      image = "<img src='" + image + "'/>";
       console.log("received " + image);
-      res.redirect("/image/" + id);
+      io.to(req.cookies.io).emit("image", { html: image });
     });
   }
 );
