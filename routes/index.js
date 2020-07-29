0af49ca6688277abdbd8090b95478f9a3ae74255
Logan Kuzyk <logan@kuzyk.net>
2020-07-28 18:01:17 -0700

    Fix redaction routes

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,22 @@ router.get("/", async (req, res, next) => {
   res.render("site/index", {
     title: "shareddit: share reddit posts with images",
     info: fs.readFileSync(__dirname + "/../views/site/info.hbs"),
+    body: fs.readFileSync(__dirname + "/../views/site/landing.hbs"),
   });
 });
 
 router.get(
-  "/r/:sub/comments/:postID/:title?/:commentID?/redact?",
+  "/r/:sub/comments/:postID/:title?/:commentID?(/redact)?",
+  async (req, res, next) => {
+    res.render("site/index", {
+      title: "shareddit: generated image",
+      info: fs.readFileSync(__dirname + "/../views/site/info.hbs"),
+    });
+  }
+);
+
+router.get(
+  "/r/:sub/comments/:postID/:title?(/redact)?",
   async (req, res, next) => {
     res.render("site/index", {
       title: "shareddit: generated image",
